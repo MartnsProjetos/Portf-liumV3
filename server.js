ac135fefee9897d44cb658cdd9b3a4b9767f01f2
Martins_dev <mtz.martinss03@gmail.com>
2025-05-27 23:59:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-aqaconst express = require('express');
+saqaconst express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 require('dotenv').config();
